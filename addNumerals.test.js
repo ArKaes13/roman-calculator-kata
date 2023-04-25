bb52c8ca6cfe5c45ad51de57cc0f5633d42cacb7
Tyler <ArKaes1820@yahoo.com>
2023-04-25 08:57:15 -0700

    Able to add all I's together

--- addNumerals.test.js
+++ addNumerals.test.js
@@ -6,3 +6,15 @@ test('adds I + I to equal II', () => {
 test('adds II + I to equal III', () => {
     expect(addNumerals('II', 'I')).toBe('III');
 });
+test('adds III + I to equal IV', () => {
+    expect(addNumerals('III', 'I')).toBe('IV');
+});
+test('adds II + II to equal IV', () => {
+    expect(addNumerals('II', 'II')).toBe('IV');
+});
+test('adds III + II to equal V', () => {
+    expect(addNumerals('III', 'II')).toBe('V');
+});
+test('adds III + III to equal VI', () => {
+    expect(addNumerals('III', 'III')).toBe('VI');
+});
