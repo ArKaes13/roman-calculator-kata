bb162a2e096c94f37460d07a8a59871d3a947c9a
Tyler <ArKaes1820@yahoo.com>
2023-04-25 14:43:06 -0700

    Able to add numerals up to XX

--- addNumerals.js
+++ addNumerals.js
@@ -1,15 +1,35 @@
-const ones = {
+const numerals = {
     I: 'I',
     II: 'II',
     III: 'III',
     IIII: 'IV',
     IIIII: 'V',
     IIIIII: 'VI',
-}
+    IIIIIII: 'VII',
+    IIIIIIII: 'VIII',
+    IIIIIIIII: 'IX',
+    IIIIIIIIII: 'X',
+    IIIIIIIIIII: 'XI',
+    IIIIIIIIIIII: 'XII',
+    IIIIIIIIIIIII: 'XIII',
+    IIIIIIIIIIIIII: 'XIV',
+    IIIIIIIIIIIIIII: 'XV',
+    IIIIIIIIIIIIIIII: 'XVI',
+    IIIIIIIIIIIIIIIII: 'XVII',
+    IIIIIIIIIIIIIIIIII: 'XVIII',
+    IIIIIIIIIIIIIIIIIII: 'XIX',
+    IIIIIIIIIIIIIIIIIIII: 'XX'
+  };
 
 function addNumerals(a, b) {
-    let sum = a + b;
-    return ones[sum];
+    let values = Object.values(numerals);
+    let keys = Object.keys(numerals);
+    let aToOnes = keys[values.indexOf(a)];
+    let bToOnes = keys[values.indexOf(b)];
+    let sum = aToOnes + bToOnes;
+
+    return numerals[sum];
 }
 
 module.exports = addNumerals;
+
--- addNumerals.test.js
+++ addNumerals.test.js
@@ -15,6 +15,50 @@ test('adds II + II to equal IV', () => {
 test('adds III + II to equal V', () => {
     expect(addNumerals('III', 'II')).toBe('V');
 });
+test('adds IV + I to equal V', () => {
+    expect(addNumerals('IV', 'I')).toBe('V');
+});
 test('adds III + III to equal VI', () => {
     expect(addNumerals('III', 'III')).toBe('VI');
 });
+test('adds IV + II to equal VI', () => {
+    expect(addNumerals('IV', 'II')).toBe('VI');
+});
+test('adds IV + III to equal VII', () => {
+    expect(addNumerals('IV', 'III')).toBe('VII');
+});
+test('adds V + II to equal VII', () => {
+    expect(addNumerals('V', 'II')).toBe('VII');
+});
+test('adds IV + IV to equal VIII', () => {
+    expect(addNumerals('IV', 'IV')).toBe('VIII');
+});
+test('adds V + III to equal VIII', () => {
+    expect(addNumerals('V', 'III')).toBe('VIII');
+});
+test('adds VI + III to equal IX', () => {
+    expect(addNumerals('VI', 'III')).toBe('IX');
+});
+test('adds V + IV to equal IX', () => {
+    expect(addNumerals('V', 'IV')).toBe('IX');
+});
+test('adds V + V to equal X', () => {
+    expect(addNumerals('V', 'V')).toBe('X');
+});
+test('adds VIII + II to equal X', () => {
+    expect(addNumerals('VIII', 'II')).toBe('X');
+});
+test('adds VI + V to equal XI', () => {
+    expect(addNumerals('VI', 'V')).toBe('XI');
+});
+test('adds IX + II to equal XI', () => {
+    expect(addNumerals('IX', 'II')).toBe('XI');
+});
+test('adds VI + VI to equal XII', () => {
+    expect(addNumerals('VI', 'VI')).toBe('XII');
+});
+test('adds X + II to equal XII', () => {
+    expect(addNumerals('X', 'II')).toBe('XII');
+});
+
+
