6be2c950fde3dc71778b04a5ed7b4a7967e9c00f
Tyler <ArKaes1820@yahoo.com>
2023-04-25 08:51:52 -0700

    II + I = III

--- addNumerals.test.js
+++ addNumerals.test.js
@@ -3,3 +3,6 @@ const addNumerals = require('./addNumerals.js');
 test('adds I + I to equal II', () => {
   expect(addNumerals('I', 'I')).toBe('II');
 });
+test('adds II + I to equal III', () => {
+    expect(addNumerals('II', 'I')).toBe('III');
+});
